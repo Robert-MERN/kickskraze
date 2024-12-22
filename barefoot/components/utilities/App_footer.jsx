ce77ed530a3aca3473cc02a00c86e7aa94f95820
Your Name <muneebsafeer@gmail.com>
2024-12-23 02:43:42 +0500

    All Fixed

--- barefoot/components/utilities/App_footer.jsx
+++ barefoot/components/utilities/App_footer.jsx
@@ -6,6 +6,7 @@ import { FiShoppingCart } from "react-icons/fi";
 import { IoGridOutline } from "react-icons/io5";
 import Badge from "@mui/material/Badge";
 import useStateContext from '@/context/ContextProvider';
+import Link from 'next/link';
 
 
 
@@ -23,32 +24,40 @@ const App_footer = () => {
     return (
         <div className='fixed bottom-0 right-0 left-0 w-screen h-[65px] px-[20px] bg-white z-20 flex pb-[5px] items-end justify-between lg:hidden   shadow-[0_0_9px_#0000001f]' >
 
-            <button className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
-                <HiOutlineHome className='text-[22px] text-stone-700' />
-                <p>Home</p>
-            </button>
+            <Link href="/" >
+                <button className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
+                    <HiOutlineHome className='text-[22px] text-stone-700' />
+                    <p>Home</p>
+                </button>
+            </Link>
+
 
             <button onClick={() => toggle_drawer("search_drawer")} className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
                 <SearchIcon className='text-stone-700' />
                 <p>Search</p>
             </button>
 
-            <button className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
-                <IoGridOutline className='text-[22px] text-stone-700' />
-                <p>Collection</p>
-            </button>
+
+            <Link href="/collection" >
+                <button className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
+                    <IoGridOutline className='text-[22px] text-stone-700' />
+                    <p>Collection</p>
+                </button>
+            </Link>
 
             <button onClick={() => toggle_drawer("menu_drawer")} className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
                 <RiAccountCircleLine className='text-[22px] text-stone-700' />
                 <p>Account</p>
             </button>
 
-            <button onClick={() => toggle_drawer("sort_drawer")} className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
-                <Badge className='badge-2' badgeContent={0} color="info" showZero>
-                    <FiShoppingCart className='text-[22px] text-stone-700' />
-                </Badge>
-                <p>Cart</p>
-            </button>
+            <Link className='/cart' >
+                <button className='text-[12px] text-stone-950 flex flex-col items-center gap-1 active:opacity-70 transition-all'>
+                    <Badge className='badge-2' badgeContent={0} color="info" showZero>
+                        <FiShoppingCart className='text-[22px] text-stone-700' />
+                    </Badge>
+                    <p>Cart</p>
+                </button>
+            </Link>
 
         </div>
     )
